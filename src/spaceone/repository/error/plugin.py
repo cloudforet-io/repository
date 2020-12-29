6078280445f2203135a4e560f7be73c05df465eb
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-30 01:06:28 +0900

    Fix a get policy bug
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>
    (cherry picked from commit cc45bfc11db30636fd087634f2ee26acbbae801a)

--- src/spaceone/repository/error/plugin.py
+++ src/spaceone/repository/error/plugin.py
@@ -2,7 +2,7 @@ from spaceone.core.error import *
 
 
 class ERROR_NO_PLUGIN(ERROR_BASE):
-    _message = 'Plugin: {plugin_id} does not exists.'
+    _message = 'Plugin does not exists. (plugin_id = {plugin_id})'
 
 
 class ERROR_INVALID_TEMPLATE_SCHEMA(ERROR_INVALID_ARGUMENT):
