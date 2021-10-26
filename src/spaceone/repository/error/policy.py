b9ff2313e4f47a426f1e5256edce757d38e50832
Gwangil Kang <gikang@mz.co.kr>
2021-10-26 13:16:12 +0900

    feat: add policy_id in create verb

--- src/spaceone/repository/error/policy.py
+++ src/spaceone/repository/error/policy.py
@@ -3,3 +3,10 @@ from spaceone.core.error import *
 
 class ERROR_NO_POLICY(ERROR_BASE):
     _message = 'Policy does not exists. (policy_id = {policy_id})'
+
+
+class ERROR_INVALID_POLICY_ID_FORMAT(ERROR_BASE):
+    _message = 'Wrong policy id. Only lower-case/number/hyphen character can be used. (policy_id = {policy_id})'
+
+class ERROR_INVALID_POLICY_ID_LENGTH(ERROR_BASE):
+    _message = 'The length of policy_id should be less than equal {length} in {policy_id}'
