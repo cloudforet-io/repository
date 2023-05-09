4fe584380e1a1a2bfe1e1d83c51579184605a32c
G.J.LEE <glee@mz.co.kr>
2023-05-09 11:11:07 +0900

    fix: get VERSION from env var
    Signed-off-by: G.J.LEE <glee@mz.co.kr>

--- src/setup.py
+++ src/setup.py
@@ -14,15 +14,12 @@
 #   limitations under the License.
 
 
+import os
 from setuptools import setup, find_packages
-
-with open('VERSION', 'r') as f: 
-    VERSION = f.read().strip()
-    f.close()
 
 setup(
     name='spaceone-repository',
-    version=VERSION,
+    version=os.environ.get('PACKAGE_VERSION'),
     description='SpaceONE repository service',
     long_description='',
     url='https://www.spaceone.dev/',
