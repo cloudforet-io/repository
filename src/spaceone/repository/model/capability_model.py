f376094775852ac491ae3905f2d62f0c05bc0531
jiyungSong <bluese@megazone.com>
2023-06-02 17:11:13 +0900

    chore: modify Capability model

--- src/spaceone/repository/model/capability_model.py
+++ src/spaceone/repository/model/capability_model.py
@@ -4,3 +4,6 @@ from schematics.types import ListType, StringType, BooleanType
 
 class Capability(Model):
     supported_schema = ListType(StringType())
+    use_resource_secret = BooleanType(default=False)
+    monitoring_type = StringType(choices=('METRIC', 'LOG'))
+    supported_providers = ListType(StringType())
